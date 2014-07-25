0f9e4eb21f36cd804d5392b0fc360da29023c35b
Jazastry <yogib@mail.bg>
2014-07-25 14:04:07 +0300

    Repared Coflict.

--- BattleShips-Main/js/main.js
+++ BattleShips-Main/js/main.js
@@ -2,6 +2,7 @@ var clicktElement;
 var clicketParent;
 var shipsOrdin = ['horizont', 'vertical'];
 var playerField = [];
+var computerField = [];
 var playerShips = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1];
 var playerShipOrientation = 1;
 var currentShip = 0;
@@ -14,18 +15,22 @@ function myFunction(e) {
 	clicktElement = e.target.id;
     clicketParent = e.target.parentNode.id;
 
-    //alert('counter' + counter);
     checkForMistakes(clicktElement);
-    if ((counter < 11)) {
+
+    if ((counter < 11) ) {
 
 	    shipDisplay(playerShips[currentShip], clicktElement, shipsOrdin[playerShipOrientation]);
+
 	    createPlayFields();
+
 	}
 }
 
 function checkForMistakes(cl) {
 
 	var right;
+
+
 	if(playerShipOrientation == 0) {
     //ne shvanah mnogo kak vzimash value-to na playerShipOrientation,
     // ama she pravq proverkite pri horizontalno polagane
@@ -52,7 +57,7 @@ function checkForMistakes(cl) {
             }
         }
 
-         else if(currentShip == 1 || currentShip == 2) {
+         else if(currentShip == 1) {
 
              if (cl == 11 || cl == 23 || cl == 35 || cl == 47 || cl == 59 ||
                  cl == 71 || cl == 95 || cl == 107 || cl == 119 || cl == 131 ||
@@ -86,6 +91,8 @@ function checkForMistakes(cl) {
          }
 
     }
+
+
 	return right;
 
     if(playerShipOrientation == 1) {
@@ -148,64 +155,63 @@ function checkForMistakes(cl) {
 
     }
         return right;
-    }
+}
 
 
 function createPlayFields() {
-	//alert('initial');
+
 
 	initialPLayerField();
 }
 
-//:::::::::::::::::::::::::::::::
+function initialPLayerField(){		
 
-
-	function initialPLayerField(){		
-
+	if (counter == 0) {	
 		for (var i = 0; i < 144; i++) {
 	
 			playerField[i] = 0;
 		};
+	};	
 
-			showPrompt(playerShips[counter]);		
-		
-	}
-	function showPrompt(length) {
+		showPrompt(playerShips[counter]);		
+	
+}
 
-		if(length == 1){
+function showPrompt(length) {
 
-			document.getElementById('playerFieldPrompt').style.display = 'block';
-			document.getElementById('0c').style.display = 'none';
-			document.getElementById('shipType').style.display = 'none';
-			document.getElementById('directionP').innerHTML = 'next ship is ' + length + 'cell';
-			document.getElementById('1c').innerHTML = 'OK';
-		} else {
+	if(length == 1){
 
 		document.getElementById('playerFieldPrompt').style.display = 'block';
-		//document.getElementById('cover').style.display = 'block';
-		document.getElementById('shipType').innerHTML = 'ship length : ' + length + ' cells';
-		}
-	}	
-		
-	function playerShipOrient(e) {
+		document.getElementById('0c').style.display = 'none';
+		document.getElementById('shipType').style.display = 'none';
+		document.getElementById('directionP').innerHTML = 'next ship is ' + length + 'cell';
+		document.getElementById('1c').innerHTML = 'OK';
+	} else {
 
-		var clickElem = e.target.id;
-
-		playerShipOrientation = parseInt(clickElem);
-
-		//alert('curr Ship ' +  currentShip + 'counter ' + counter);
-
-		document.getElementById('playerFieldPrompt').style.display = 'none'; 
-
-		if( counter != 0) {
-
-		currentShip += 1;		
-		}
-		
-		counter += 1;
-
-		return;
+	document.getElementById('playerFieldPrompt').style.display = 'block';
+	//document.getElementById('cover').style.display = 'block';
+	document.getElementById('shipType').innerHTML = 'ship length : ' + length + ' cells';
 	}
+}	
+	
+function playerShipOrient(e) {
+
+	var clickElem = e.target.id;
+
+	playerShipOrientation = parseInt(clickElem);
+
+	document.getElementById('playerFieldPrompt').style.display = 'none'; 
+
+	if( counter != 0) {
+
+	   currentShip += 1;	
+
+	}
+	
+	counter += 1;
+
+	return;
+}
 
 
 //:::::::::::::::::::
@@ -217,10 +223,22 @@ function shipDisplay(length, startElement, shipOrdin){
 
 		if(length == 4) {
 
+			//alert(playerField);
+
 			document.getElementById(startElement).style.backgroundImage = "url('images/shipBack.png')";
 			document.getElementById(parseInt(startElement)+1).style.backgroundImage = "url('images/shipMiddle.png')";
 			document.getElementById(parseInt(startElement)+2).style.backgroundImage = "url('images/shipMiddle.png')";
 			document.getElementById(parseInt(startElement)+3).style.backgroundImage = "url('images/shipFront.png')";
+
+		
+			palyerField[startElement] = 1;
+			palyerField[startElement + 1] = 1;
+			palyerField[startElement + 2] = 1;
+			palyerField[startElement + 3] = 1;
+
+			alert(playerField);
+
+
 		} else if (length == 3) {
 
 			document.getElementById(startElement).style.backgroundImage = "url('images/shipBack.png')";
@@ -257,7 +275,10 @@ function shipDisplay(length, startElement, shipOrdin){
 	}
 }
 
+function computerFieldCreation() {
 
+
+}
 //scoreboard  func
-document.getElementById('player1').value = player1score + " / " + 20;
-document.getElementById('player2').value = player2score + " / " + 20;
+// document.getElementById('player1').value = player1score + " / " + 20;
+// document.getElementById('player2').value = player2score + " / " + 20;
